9d9320fc5e1265e4f850b3c26f73224a85e6ffb9
Tavo Cruz <madecruz2@gmail.com>
2023-11-08 17:05:28 -0800

    this is correct

--- src/components/ConditionalRendering.js
+++ src/components/ConditionalRendering.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react'
 
 const ConditionalRendering = ({ id }) => {
-  //sometimes the id may exist and sometimes it may not and exit with the return inside the if
-  if (!id) {
-    return "No id provided";
-  }
   //this causes an error with the useState and useEffect
   const [something, setSomething] = useState("testing");
   useEffect(() => {
     
   }, [something]);
+  //sometimes the id may exist and sometimes it may not and exit with the return inside the if
   return (
-    <section>{/* Product card */ }</section>
+    <section>
+      {
+        !id
+          ?"No id provided"
+          :{/* Product card */ }
+      }
+    </section>
   )
 }
 
