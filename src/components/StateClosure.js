e0f7e3a1dac3c848c809a6056640f104db734a00
Tavo Cruz <madecruz2@gmail.com>
2023-11-09 09:36:17 -0800

    counter works fine with the state closure

--- src/components/StateClosure.js
+++ src/components/StateClosure.js
@@ -4,11 +4,16 @@ const StateClosure = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const i = setInterval(() => {
       console.log("Interval function is running...");
       setCount(count + 1);
     }, 1000);
+    //useEffect clean up function from prev useEffect run
+    return () => {
+      clearInterval(i);
+    }
   }, [count])
+
   return (
     <p>Count is: {count}</p>
   )
