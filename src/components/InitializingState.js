f2df1761272fffb06229fe40f394fdd5225558f6
Tavo Cruz <madecruz2@gmail.com>
2023-11-09 08:39:13 -0800

    One way of solving it using optional chaining

--- src/components/InitializingState.js
+++ src/components/InitializingState.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 //initializing state with an object
 const InitializingState = () => {
-  const [post, setPost] = useState();
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
     fetch("https://dummyjson.com/posts/1")
@@ -11,10 +11,11 @@ const InitializingState = () => {
       });
   }, []);
   //remember useEffect runs after rendering so we will first render this return block then run useEffect hence the values to post are undefined since i have now set any initial values for them in the useState
+  //to fix it use optional chanining or initialize the useState with null
   return (
     <article>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
     </article>
   )
 }
